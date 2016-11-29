5152b0bf93c392b3b43077deb74c23dbaebd7908
Samuel Rodríguez Medina <samuelyret@gmail.com>
2016-11-29 17:41:59 +0100

    Update conceptual_book.rb

--- app/models/conceptual_book.rb
+++ app/models/conceptual_book.rb
@@ -1,10 +1,9 @@
 class ConceptualBook < ApplicationRecord
   belongs_to :genre
   has_many :physical_books
-  has_many :wishlist_item
+  has_many :wishlist_items
   validates :title, presence: true
   validates :author, presence: true
-  validates :isbn, presence: true, uniqueness: true, numericality: true
+  validates :isbn, presence: true, uniqueness: true, numericality: true, length: { maximum: 13 }
   validates :description, presence: true
-  validates :cover_picture_url, presence: true
 end

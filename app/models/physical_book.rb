12a9f01c393aad9125959de75b0a18fd6a6e455f
Jasper Hendrickx <jasper_hendrickx@hotmail.com>
2016-11-29 17:34:01 +0100

    Create physical book validations

--- app/models/physical_book.rb
+++ app/models/physical_book.rb
@@ -1,4 +1,8 @@
 class PhysicalBook < ApplicationRecord
   belongs_to :user
   belongs_to :conceptual_book
+  has_many :transactions
+  validates :status, presence: true
+  validates :picture_url, presence: true
+  validates :price, presence: true, numericality: true
 end

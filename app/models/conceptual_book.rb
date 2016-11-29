33c131417eb8b8656583001053bff2c47d209ea1
Samuel Horn <samuelhorn@gmail.com>
2016-11-29 17:34:24 +0100

    create validation for conceptual book model

--- app/models/conceptual_book.rb
+++ app/models/conceptual_book.rb
@@ -1,3 +1,10 @@
 class ConceptualBook < ApplicationRecord
   belongs_to :genre
+  has_many :physical_books
+  has_many :wishlist_item
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :isbn, presence: true, uniqueness: true, numericality: true
+  validates :description, presence: true
+  validates :cover_picture_url, presence: true
 end

d015232caa952e4538ca199872ef3c726354dbf5
Samuel Horn <samuelhorn@gmail.com>
2016-11-30 16:12:59 +0100

    add search magic (search form) to physical_book index.html.erb

--- app/models/physical_book.rb
+++ app/models/physical_book.rb
@@ -4,4 +4,16 @@ class PhysicalBook < ApplicationRecord
   validates :status, presence: true
   validates :picture_url, presence: true
   validates :price, presence: true, numericality: true
+
+  # include PgSearch
+  # pg_search_scope :search, against: [:title, :author, :description],
+  #   using: {tsearch: {dictionary: "english"}},
+  #   associated_against: {genre: :name}
+
+  def self.text_search(query)
+    if query.present?
+      where("title @@ :q or description @@ :q or author @@ :q", q: query)
+
+    end
+  end
 end

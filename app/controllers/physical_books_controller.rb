280aef40f9add023210458b40ccb3e0abe6cec86
Jasper Hendrickx <jasper_hendrickx@hotmail.com>
2016-11-30 16:53:56 +0100

    Save to database completed

--- app/controllers/physical_books_controller.rb
+++ app/controllers/physical_books_controller.rb
@@ -1,6 +1,6 @@
 class PhysicalBooksController < ApplicationController
   def index
-    @physical_books=PhysicalBook.all
+    @physical_books = PhysicalBook.all
   end
 
   def new
@@ -8,12 +8,19 @@ class PhysicalBooksController < ApplicationController
   end
 
   def create
-    @new_physical_book = PhysicalBook.new
-    @new_physical_book.save
-    redirect_to physical_books_path
+    @new_physical_book = PhysicalBook.new(physical_book_params)
+    @new_physical_book.user = User.first
+    if @new_physical_book.save!
+      redirect_to physical_books_path
+    else
+      render :new
+    end
   end
 
-
+private
+  def physical_book_params
+    params.require(:physical_book).permit(:title, :author, :status, :cover_pic_url, :price)
+  end
 
 
 end
--- app/models/physical_book.rb
+++ app/models/physical_book.rb
@@ -2,6 +2,6 @@ class PhysicalBook < ApplicationRecord
   belongs_to :user
   has_many :transactions
   validates :status, presence: true
-  validates :picture_url, presence: true
-  validates :price, presence: true, numericality: true
+  # validates :picture_url, presence: true
+  validates :price, presence: true
 end

c570f7a6539f5b97852074cbcf4ab61f8dcf1b76
Samuel Rodriguez <samuelyret@gmail.com>
2016-12-02 16:32:09 +0100

    added users to routes, nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   devise_for :users
   devise_for :views
-  resources 'physical_books'
-  get 'users/:id' => 'users#show'
 
-  root 'static_pages#home'
-
+  resources 'users' do
+    resources 'physical_books'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+

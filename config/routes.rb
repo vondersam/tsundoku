15e809d44113b966adbc704ce0fdeec411f5df10
Samuel Rodriguez <samuelyret@gmail.com>
2016-12-02 16:41:43 +0100

    added nested route for user

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :views
 
-  resources 'users' do
-    resources 'physical_books'
+  resources 'physical_books' do
+    resources 'users'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

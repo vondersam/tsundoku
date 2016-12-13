c5ce0d48e5d0ea74a22819a8ca96ae512fa2d0fb
Samuel Horn <samuelhorn@gmail.com>
2016-12-13 13:39:38 +0100

    delete some lines not needed for the map and removed the map and distance for current user profile

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,13 +4,8 @@ class StaticPagesController < ApplicationController
 
   def home
     @physical_books = PhysicalBook.all
-    @result = request.location #CHECK WITH THOMAS
-    @users = User.where.not(latitude: nil, longitude: nil) #comes from the LeWagon template on web
-    # @users_coordinates = []  #adapted from Karr
-    # @users.each do |user|    #adapted from Karr
-    #   user_coordinates = { lat: user.latitude, lng: user.longitude } #adapted from Karr
-    #   @users_coordinates << user_coordinates    #adapted from Karr
-    # end
+    @result = request.location
+    @users = User.where.not(latitude: nil, longitude: nil)
 
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude

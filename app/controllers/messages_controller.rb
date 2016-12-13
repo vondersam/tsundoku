5d3d3602091900e998966e2517bdf2b53af197aa
Jasper Hendrickx <jasper_hendrickx@hotmail.com>
2016-12-13 17:51:25 +0100

    Redo changes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,12 +3,14 @@ class MessagesController < ApplicationController
 
   def new
     @chosen_recipient = User.find_by(id: params[:to].to_i) if params[:to]
+    @book = PhysicalBook.find(params[:book])
   end
 
   def create
-    recipients = User.where(id: params['recipients'])
+    recipients = User.find(params['recipients'])
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
     flash[:success] = "Message has been sent!"
+
     redirect_to conversation_path(conversation)
   end
 end

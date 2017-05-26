d96668b0912d280a885c9f11086c7718025891cd
Samuel Rodriguez <samuelyret@gmail.com>
2017-05-26 22:02:38 +0100

    no price sign if no price in the book

--- app/helpers/physical_books_helper.rb
+++ app/helpers/physical_books_helper.rb
@@ -1,9 +1,9 @@
 module PhysicalBooksHelper
   def display_book_price(book)
     if book.price > 0
-      "#{book.price}"
+      "| € #{book.price}"
     else
-      book.price
+      ""
     end
   end
 end
